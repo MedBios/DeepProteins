1bd6824ae770854aafca8245adfff03d92bbd042
Rachel A St Clair <rachel.aileen.st@gmail.com>
2018-10-22 19:31:25 -0400

    IT WORKS --> iterable save file

--- SOMMMP9/SOMMMP9-4.py
+++ SOMMMP9/SOMMMP9-4.py
@@ -16,16 +16,13 @@ import cv2
 from scipy.misc import bytescale
 from mpl_toolkits.mplot3d import Axes3D
 import time
-import progressbar
+import csv
 
 
 down = []
 up = []
 neut = []
-#get the data without the first column as integers
-bar = progressbar.ProgressBar()
-
-filename='Data/DE0.csv'
+filename='DE0.csv'
 data = np.genfromtxt(filename, delimiter=',', missing_values='NA', skip_header=2, filling_values=1, usecols=range(1,7))
 removeNA = data[:, -1] != 1
 data = data[removeNA, :]
@@ -48,46 +45,71 @@ number_nodes = 3
 w = np.random.randn(number_nodes, n_in) * 0.1
 #hyperparameters
 lr = 0.025
-n_iters = 600
+n_iters = 400
 #do the training and show the weights on the nodes with cv2
-def trial(n_trails):
-    for i in range(n_iters):
-        randsamples = np.random.randint(0, data.shape[0], 1)[0] 
-        rand_in = data[randsamples, :] 
-        difference = w - rand_in
-        dist = np.sum(np.absolute(difference), 1)
-        best = np.argmin(dist)
-        w_eligible = w[best,:]
-        w_eligible += (lr * (rand_in - w_eligible))
-        w[best,:] = w_eligible
-        cv2.namedWindow('weights', cv2.WINDOW_NORMAL)
-        cv2.imshow('weights', bytescale(w))
-        cv2.waitKey(100)  
+def trial(n_trial):
+    for instance in range(n_trial): 
+        for i in range(n_iters):
+            randsamples = np.random.randint(0, data.shape[0], 1)[0] 
+            rand_in = data[randsamples, :] 
+            difference = w - rand_in
+            dist = np.sum(np.absolute(difference), 1)
+            best = np.argmin(dist)
+            w_eligible = w[best,:]
+            w_eligible += (lr * (rand_in - w_eligible))
+            w[best,:] = w_eligible
+            cv2.namedWindow('weights', cv2.WINDOW_NORMAL)
+            cv2.imshow('weights', bytescale(w))
+            cv2.waitKey(100)
+        cv2.destroyAllWindows()
 #query validation   
-    for node in w:
-        while w[0, 1] < 0.5:
-            node1 = w[0, :]
-            difference1 = node1 - data
-            dist1 = np.sum(np.abs(difference1), 1)
-            top1 = np.argmin(dist1)
-            ans1 = namedata[top1]
-            down.insert(0, ans1) 
-        while w[1, 1] > 0.5:
-            node2 = w[1, :]
-            difference2 = node2 - data
-            dist2 = np.sum(np.abs(difference2), 1)
-            top2 = np.argmin(dist2)
-            ans2 = namedata[top2]
-            up.insert(0, ans2)
-        else:
-            node3 = w[2, :]
-            difference3 = node3 - data
-            dist3 = np.sum(np.abs(difference3), 1)
-            top3 = np.argmin(dist3)
-            ans3 = namedata[top3]
-            neut.insert(0, ans3)
+        logcol = w[:, 1]
+        downcol = np.argmin(logcol)
+        upcol = np.argmax(logcol)
+        
+        node1 = w[downcol, :]
+        node2 = w[upcol, :]
+        node3 = np.delete(w, [downcol, upcol], axis=0)               
+
+        difference1 = node1 - data
+        dist1 = np.sum(np.abs(difference1), 1)
+        top1 = np.argmin(dist1)
+        ans1 = namedata[top1]
+        print(ans1)
+        down.insert(0, ans1) 
+        difference2 = node2 - data
+        dist2 = np.sum(np.abs(difference2), 1)
+        top2 = np.argmin(dist2)
+        ans2 = namedata[top2]
+        up.insert(0, ans2)
+        difference3 = node3 - data
+        dist3 = np.sum(np.abs(difference3), 1)
+        top3 = np.argmin(dist3)
+        ans3 = namedata[top3]
+        neut.insert(0, ans3)
 #show the weights for the datafile
     print (filename)
+    print(w)
+    print(node1, node2, node3)
     print(down, up, neut)
-trial(1)
+n_trial = 1
+trial(n_trial)
+csvfile = 'genes' + str(n_trial) + 'k.csv'
+csvname = 'genes' + str(n_trial)
+with open(csvfile, mode='w', newline='') as csvname:
+    gene_writer = csv.writer(csvname, delimiter=',')
+    gene_writer.writerow(down)
+    gene_writer.writerow(up)
+    gene_writer.writerow(neut)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 ###############################################################################
